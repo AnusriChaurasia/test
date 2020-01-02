f591cb940495598fe8613e61c99514648b137b16
Anusri Chaurasia <35715688+AnusriChaurasia@users.noreply.github.com>
2020-01-02 14:55:53 +0530

    Add files via upload

---
+++ folium-1.py
@@ -0,0 +1,9 @@
+'''import folium 
+world_map=folium.map(location=[56.130,-106.35], zoom_start=4,title="Stamen tones")
+world_map
+Ontario=folium.map.FeatureGroup()
+
+from socket import socket'''
+import folium
+m = folium.Map(location=[45.5236, -122.6750], zoom_start=4, title='Stamen Terrain')
+m
